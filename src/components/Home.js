850ae2faebeff305ca24a793ceac1f20cd3670e6
kannankkking <nagappanramu44@gmail.com>
2025-07-14 07:41:50 +0530

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
             <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
               Hi, I'm <span className="bg-gradient-to-r from-emerald-400 to-violet-500 bg-clip-text text-transparent">Kannan N</span>
             </h1>
-            <h2 className="text-2xl md:text-3xl text-slate-300 mb-6">Full Stack Developer</h2>
+            <h2 className="text-2xl md:text-3xl text-slate-300 mb-6">Full Stack Web Developer</h2>
             <h1><span className="text-blue-500 text-2xl"> Tech Stack:</span> React.Js, TailwindCss,  Node.Js, Express.Js, MongoDB</h1>
             
             <p className="text-slate-400 text-lg mt-5">
@@ -20,13 +20,13 @@ const Home = () => {
 
           <div className="order-1 lg:order-2 flex justify-center" data-aos="fade-left">
             <div className="relative w-full flex justify-center">
-              <div className="w-96 h-96 sm:w-[50%] sm:h-112 md:w-144 md:h-144 lg:w-160 lg:h-160 xl:w-192 xl:h-192 2xl:w-224 2xl:h-224 rounded-full overflow-hidden border-4 border-slate-700 relative z-10">
+              <div className="w-96 h-96 sm:w-[50%] sm:h-[500px] md:w-[500px] border-blue-950 md:h-144 lg:w-160 lg:h-160 xl:w-192 xl:h-192 2xl:w-224 2xl:h-224 rounded-full overflow-hidden border-4 border-slate-700 relative">
                 <img
                   src={profile}
                   alt="Developer Profile"
                   className="w-full h-full object-cover"/>
               </div>
-              <div className="absolute inset-0 bg-gradient-to-br from-emerald-500 to-violet-500 rounded-full blur-xl opacity-10 animate-pulse"></div>
+              <div className="absolute inset-0 bg-gradient-to-br  rounded-full blur-xl opacity-50 animate-pulse"></div>
             </div>
           </div>
         </div>
